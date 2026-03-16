59d4763f6732d4c1b07c2622cc467211b3b27297
Swati <swatik2706@gmail.com>
2026-03-16 23:14:43 +0530

    Time: 29 ms (41.97%), Space: 17.4 MB (45.62%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,43 +1,31 @@
 class Solution {
 public:
     int numIslands(vector<vector<char>>& grid) {
-        int rows = grid.size();
-        int cols = grid[0].size();
-        vector<vector<int>> visited(rows, vector<int>(cols,0));
+        int m = grid.size();
+        int n = grid[0].size();
+        vector<vector<int>> visited(m, vector<int>(n,0));
         int cnt=0;
-        for(int i=0;i<rows;i++){
-            for(int j=0;j<cols;j++){
-                if(!visited[i][j] && grid[i][j]=='1'){
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]=='1' && !visited[i][j]){
                     cnt++;
-                    bfs(i,j,visited,grid);
+                    dfs(i,j,visited,grid);
                 }
             }
         }
         return cnt;
     }
 
-    void bfs(int row, int col, vector<vector<int>>& visited, vector<vector<char>>& grid){
+    void dfs(int row, int col, vector<vector<int>>& visited, vector<vector<char>>& grid){
+        int m = grid.size();
+        int n = grid[0].size();
+
+        if(row>=m || row<0 || col>=n || col<0 || grid[row][col]=='0' || visited[row][col]) return;
+
         visited[row][col] = 1;
-        queue<pair<int,int>> q;
-        int rows = grid.size();
-        int cols = grid[0].size();
-        vector<int> delrow = {-1,0,1,0};
-        vector<int> delcol = {0,1,0,-1};
-        q.push({row,col});
-        while(!q.empty()){
-            int row = q.front().first;
-            int col = q.front().second;
-            q.pop();
-
-            for(int i=0;i<4;i++){
-                int nrow = row + delrow[i];
-                int ncol = col + delcol[i];
-                if(nrow >= 0 && nrow<rows && ncol >= 0 && ncol<cols && !visited[nrow][ncol] && grid[nrow][ncol]=='1'){
-                    visited[nrow][ncol] = 1;
-                    q.push({nrow, ncol});
-                }
-
-            }
-        }
+        dfs(row-1,col,visited,grid);
+        dfs(row+1,col,visited,grid);
+        dfs(row,col-1,visited,grid);
+        dfs(row,col+1,visited,grid);
     }
 };
