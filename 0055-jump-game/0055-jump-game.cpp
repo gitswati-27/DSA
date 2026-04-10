5f7ecf546235d7ff0a0004ce63e960a81e1e737b
Swati <swatik2706@gmail.com>
2026-04-10 20:11:03 +0530

    Time: 0 ms (100%), Space: 52.4 MB (24.18%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -3,22 +3,14 @@ public:
     bool canJump(vector<int>& nums) {
         if(nums.size()==1) return true;
         if(nums[0]==0) return false;
+        int maxi = nums[0];
         int n=nums.size(), pos = 0;
-        while(pos<n){
-            if(nums[pos]==0) return false;
-            int next = -1, steps=-1;
-            for(int j = 1;j<=nums[pos];j++){
-                if(pos+j>=n-1) return true;
-                //if(nums[pos+j]<steps) continue;
-                int reach = pos+j+nums[pos+j];
-                if(reach>steps){
-                    next = pos+j;
-                    steps = reach;
-                }
-            }
-            pos = next;
+        for(int i=0;i<n;i++){
+            if(i>maxi) return false;
+            if(maxi >= n-1) return true;
+            int reach = i + nums[i];
+            maxi = max(maxi, reach);
         }
-        if(pos<n-1) return false;
         return true;
     }
 };
