348e1120ba58bf236195d0ce22ff09c112fd2ab4
Swati <swatik2706@gmail.com>
2026-06-15 20:15:32 +0530

    Time: 42 ms (24.69%), Space: 151.1 MB (18.87%) - LeetHub

--- 0155-min-stack/0155-min-stack.cpp
+++ 0155-min-stack/0155-min-stack.cpp
@@ -1,27 +1,40 @@
 class MinStack {
 public:
-    stack<pair<int,int>> st;
+    stack<long long> st;
+    long long mini=INT_MAX;
     MinStack() {
         
     }
     
     void push(int val) {
-        if(st.empty()) st.push({val,val});
+        if(st.empty()){
+            st.push(val);
+            mini = val;
+        }
         else{
-            st.push({val, min(val, getMin())});
+            if(val>mini) st.push(val);
+            else{
+                st.push(2LL*val - mini);
+                mini = val;
+            }
         }
     }
     
     void pop() {
+        long long x = st.top();
         st.pop();
+        if(x<mini)
+            mini = 2LL*mini - x;
     }
     
     int top() {
-        return st.top().first;
+        long long x = st.top();
+        if(x>mini) return x;
+        return mini;
     }
     
     int getMin() {
-        return st.top().second;
+        return mini;
     }
 };
 
